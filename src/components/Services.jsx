208bfd7389d8aada0d9c6c219e40f647423e90c2
Habib <yusufhabib3@gmail.com>
2025-11-28 20:00:45 +0100

    made services header more responsive and added services cards

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <section className='px-padding-sm '>
-      <div className='mt-16 flex flex-col justify-center gap-5 items-center'>
+    <section className='px-padding-sm lg:px-padding-lg '>
+      <div className='mt-16 flex flex-col justify-center gap-5 lg:gap-8 items-center lg:flex-row lg:justify-start'>
         <h1><span className='text-lar font-bold bg-primary text-center'>Services</span></h1>
-        <p className='text-center'>At our digital marketing agency, we 
+        <p className='text-center max-w-[505px] lg:text-left'>At our digital marketing agency, we 
           offer a range of services to help businesses grow and succeed online. 
           These services include:</p>
       </div>
-      <div>
+      <div className=' mt-padding-md [&>div]:border-2 [&>div]:border-b-7 [&>div]:mt-padding-sm [&>div]:h-60 [&>div]:w-full [&>div]:rounded-4xl'>
         <div></div>
         <div></div>
         <div></div>
