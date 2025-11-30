7db06a6620d6c8b1f2fe3aae099ef268b6b7adab
Habib <yusufhabib3@gmail.com>
2025-11-30 17:23:11 +0100

    added and styled services cards

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,13 +9,88 @@ const Services = () => {
           offer a range of services to help businesses grow and succeed online. 
           These services include:</p>
       </div>
-      <div className=' mt-padding-md [&>div]:border-2 [&>div]:border-b-7 [&>div]:mt-padding-sm [&>div]:h-60 [&>div]:w-full [&>div]:rounded-4xl'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className=' servicesCard-container mt-padding-md grid
+      [&>div]:border-2 [&>div]:border-b-7 [&>div]:mt-padding-sm [&>div]:h-auto [&>div]:rounded-4xl'>
+        <div className='p-8 flex bg-tertiary'>
+          <div className=' flex flex-col justify-between gap-20 min-w-1/2'>
+            <h1 className=' bg-inherit '><span className=' text-mid font-medium text-left bg-primary min-w-[200px] '>Search engine optimization</span></h1>
+            <div className='flex items-center gap-4'>
+              <div className='w-10 flex  items-center'><img src="/link-Icon.png" alt="link arrow" /></div>
+              <a href="#">Learn more</a>
+            </div>
+          </div>
+          <div className='flex  items-center min-w-1/2'>
+            <img className='w-full ' src="/Illustration1.png" alt="illustration 1" />
+          </div>
+        </div>
+        <div className='p-8 flex bg-primary'>
+          <div className=' flex flex-col justify-between gap-20 min-w-1/2'>
+            <h1 className=' bg-inherit '><span className=' text-mid font-medium text-left bg-white min-w-[200px] '>Pay-per-click advertising</span></h1>
+            <div className='flex items-center gap-4'>
+              <div className='w-10 flex  items-center'><img src="/link-Icon.png" alt="link arrow" /></div>
+              <a href="#">Learn more</a>
+            </div>
+          </div>
+          <div className='flex  items-center min-w-1/2'>
+            <img className='w-full ' src="/Illustration2.png" alt="illustration 2" />
+          </div>
+        </div>
+        <div className='p-8 flex bg-secondary'>
+          <div className=' flex flex-col justify-between gap-20 min-w-1/2'>
+            <h1 className=' bg-inherit '><span className=' text-mid font-medium text-left bg-primary min-w-[200px] '>Social Media Marketing</span></h1>
+            <div className='flex items-center gap-4'>
+              <div className='w-10 flex  items-center'><img src="/link-Icon2.png" alt="link arrow" /></div>
+              <a className='text-white' href="#">Learn more</a>
+            </div>
+          </div>
+          <div className='flex  items-center min-w-1/2'>
+            <img className='w-full ' src="/Illustration3.png" alt="illustration 3" />
+          </div>
+        </div>
+        <div className='p-8 flex bg-tertiary'>
+          <div className=' flex flex-col justify-between gap-20 min-w-1/2'>
+            <h1 className=' bg-inherit '><span className=' text-mid font-medium text-left bg-primary min-w-[200px] '>Email Marketing</span></h1>
+            <div className='flex items-center gap-4'>
+              <div className='w-10 flex  items-center'><img src="/link-Icon.png" alt="link arrow" /></div>
+              <a href="#">Learn more</a>
+            </div>
+          </div>
+          <div className='flex  items-center min-w-1/2'>
+            <img className='w-full ' src="/Illustration4.png" alt="illustration 4" />
+          </div>
+        </div>
+        <div className='p-8 flex bg-primary'>
+          <div className=' flex flex-col justify-between gap-20 min-w-1/2'>
+            <h1 className=' bg-inherit '><span className=' text-mid font-medium text-left bg-white min-w-[200px] '>Content Creation</span></h1>
+            <div className='flex items-center gap-4'>
+              <div className='w-10 flex  items-center'><img src="/link-Icon.png" alt="link arrow" /></div>
+              <a href="#">Learn more</a>
+            </div>
+          </div>
+          <div className='flex  items-center min-w-1/2'>
+            <img className='w-full ' src="/Illustration5.png" alt="illustration 5" />
+          </div>
+        </div>
+        <div className='p-8 flex bg-secondary'>
+          <div className=' flex flex-col justify-between gap-20 min-w-1/2'>
+            <h1 className=' bg-inherit '><span className=' text-mid font-medium text-left bg-primary min-w-[200px] '>Analytic and Tracking</span></h1>
+            <div className='flex items-center gap-4'>
+              <div className='w-10 flex  items-center'><img src="/link-Icon2.png" alt="link arrow" /></div>
+              <a className='text-white' href="#">Learn more</a>
+            </div>
+          </div>
+          <div className='flex  items-center min-w-1/2'>
+            <img className='w-full ' src="/Illustration6.png" alt="illustration 6" />
+          </div>
+        </div>
+      </div>
+      <div className='bg-tertiary my-padding-lg rounded-4xl p-8'>
+        <div>
+          <h1 className='text-mid font-medium'>Let's make things happen</h1>
+          <p className='mt-5'>Contact us today to learn about how our digital marketing services can help your business grow and succeed online.</p>
+          <button className='bg-secondary text-white p-2 rounded-xl mt-5 w-full md:w-2/3 h-16'>Get your proposal</button>
+        </div>
+        <div className='hidden'></div>
       </div>
     </section>
   )
