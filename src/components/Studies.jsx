87306ab82cfd24a364fbfc0f7064331e5c980019
Habib <yusufhabib3@gmail.com>
2025-12-05 03:39:04 +0100

    added case study tabs and styled scrolling effect for mobile screens

--- src/components/Studies.jsx
+++ src/components/Studies.jsx
@@ -9,10 +9,29 @@ const Studies = () => {
           Case Studies
         </p>
       </div>
-      <div className='text-white [&>div]:px-padding-sm [&>div]:py-padding-md [&>div]:bg-secondary [&>div]:rounded-4xl '>
+      <div className='case-studies-tabs grid mt-padding-md text-white w-full justify-center gap-x-4 
+      [&>div]:min-w-[280px] [&>div]:w-full [&>div]:max-w-[300px] [&>div]:px-padding-sm [&>div]:py-[55px] [&>div]:bg-secondary [&>div]:rounded-4xl '>
+        <div className='flex flex-col gap-8  '>
+          <p>For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic 
+            and a 25% increase in sales.</p>
+          <div>
+            <a href="#" className='flex items-center gap-2 text-primary '>Learn More 
+              <img className=' w-8' src="/link-Icon.png" alt="Link Icon" />
+            </a>
+          </div>
+        </div>
         <div className='flex flex-col gap-8 '>
           <p>For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords 
             and a 200% increase in organic traffic.</p>
+          <div>
+            <a href="#" className='flex items-center gap-2 text-primary '>Learn More 
+              <img className=' w-8' src="/link-Icon.png" alt="Link Icon" />
+            </a>
+          </div>
+        </div>
+        <div className='flex flex-col gap-8 '>
+          <p>For a national retail chain, we created a social media marketing campaign that increased followers by 25% 
+            and generated a 20% increase in online sales.</p>
           <div>
             <a href="#" className='flex items-center gap-2 text-primary '>Learn More 
               <img className=' w-8' src="/link-Icon.png" alt="Link Icon" />
