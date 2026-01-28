5497fb33d1b13e1d5ca300b9af71acedf0865d43
Habib <yusufhabib3@gmail.com>
2026-01-28 15:26:39 +0100

    added styling to form inputs, and button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,30 +8,38 @@ const Contact = () => {
         <p className='text-center max-w-[340px] lg:text-left'>Connect with Us: Let's Discuss Your Digital Marketing Needs
         </p>
       </div>
-      <div className='form container w-full border bg-tertiary p-padding-sm rounded-4xl '>
-        <form action="">
-          <fieldset id='radio' className='radio flex justify-between' >
+      <div className='form-container mt-padding-md'>
+        <form action="" className=' w-full border bg-tertiary p-padding-sm rounded-4xl '>
+          <fieldset id='radio' className='radio flex justify-evenly' >
             <label name='hi'>
-              <input type="radio" name="contact-type" id="say-hi" defaultChecked/> Say Hi!
+              <input type="radio" name="contact-type" id="say-hi" defaultChecked /> Say Hi!
             </label>
             <label name='qoute'>
               <input type="radio" name="contact-type" id="get-a-quote" /> Get a Quote
             </label>
           </fieldset>
-          <fieldset className='border'>
-            <label htmlFor="">Name 
-              <input type="text" name="name" id="email" />
+          <fieldset className=' grid grid-cols-1 gap-6 py-8'>
+            <label htmlFor="contact" className='grid grid-cols-1 w-full gap-2.5 [&>input]:w-full [&>input]:outline-none [&>input]:border [&>input]:bg-white
+            [&>input]:rounded-sm [&>input]:h-11 [&>input]:p-5 ' >Name
+              <input type="text" name="name" id="name" placeholder='Name' />
             </label>
-            <label htmlFor=""> Email*
-              <input type="email" name="email" id="email" />
+            <label htmlFor="contact" className='grid grid-cols-1 w-full gap-2.5 [&>input]:w-full [&>input]:outline-none [&>input]:border [&>input]:bg-white
+            [&>input]:rounded-sm [&>input]:h-11 [&>input]:p-5 ' >Email*
+              <input type="email" name="email" id="email" placeholder='Email' />
             </label>
-            <label htmlFor="">Message*
-              <textarea name="message" id="message"></textarea>
+            <label htmlFor="contact" className='grid grid-cols-1 w-full gap-2.5 [&>textarea]:w-full [&>textarea]:outline-none [&>textarea]:border [&>textarea]:bg-white
+            [&>textarea]:rounded-sm [&>textarea]:h-22 [&>textarea]:p-5  '>Message*
+              <textarea name="message" id="message" placeholder='Message' ></textarea>
             </label>
           </fieldset>
-
-
         </form>
+          <div className="btn-container w-full flex justify-center">
+            <button
+              className='h-16 w-full md:w-2/3 lg:max-w-[250px] mt-5 self-center lg:self-start
+                        p-2 px-8 rounded-xl border-[1.5px] md:self-center text-white bg-secondary'
+            >Send a Message
+            </button>
+          </div>
       </div>
 
     </section>
