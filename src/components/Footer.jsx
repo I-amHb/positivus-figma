68e0e0cd3d55a7bc120ce221532371a0c6fca627
Habib <yusufhabib3@gmail.com>
2026-04-30 13:39:47 +0100

    added tailwind styling to svg and icon color changing effect on hover to social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='mt-padding-md lg:mx-padding-lg '>
-      <div className='w-full pt-padding-md px-padding-sm bg-secondary text-white lg:rounded-t-4xl'>
+      <div className='w-full pt-padding-md px-padding-md bg-secondary text-white lg:rounded-t-4xl'>
         <div className='footer-header lg:flex lg:justify-between lg:items-center '>
           <div className='w-full flex justify-center items-center gap-2 
           lg:justify-normal lg:w-auto'>
@@ -23,7 +23,7 @@ const Footer = () => {
 
           <div className="socials-list hidden gap-3 justify-center items-center mt-5 lg:flex lg:items-center lg:mt-0">
             <a href='#' className='linkedIn '>
-              <svg fill="#fff" height="26" width="26" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+              <svg className='w-9 h-9 fill-current text-white hover:text-primary transition-all duration-500 delay-100 ' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                 viewBox="-143 145 512 512" xml:space="preserve">
                 <path d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M41.4,508.1H-8.5V348.4h49.9
 	V508.1z M15.1,328.4h-0.4c-18.1,0-29.8-12.2-29.8-27.7c0-15.8,12.1-27.7,30.5-27.7c18.4,0,29.7,11.9,30.1,27.7
@@ -33,12 +33,12 @@ const Footer = () => {
               </svg>
             </a>
             <a href='#' className='facebook '>
-              <svg fill="#fff" width="26" height="26" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <svg className='w-9 h-9 fill-current text-white hover:text-primary transition-all duration-500 delay-100 ' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 2.03998C6.5 2.03998 2 6.52998 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.84998C10.44 7.33998 11.93 5.95998 14.22 5.95998C15.31 5.95998 16.45 6.14998 16.45 6.14998V8.61998H15.19C13.95 8.61998 13.56 9.38998 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96C15.9164 21.5878 18.0622 20.3855 19.6099 18.57C21.1576 16.7546 22.0054 14.4456 22 12.06C22 6.52998 17.5 2.03998 12 2.03998Z" />
               </svg>
             </a>
             <a href='#' className='twitter '>
-              <svg fill="#fff" height="26" width="26" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+              <svg className='w-9 h-9 fill-current text-white hover:text-primary transition-all duration-500 delay-100 ' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                 viewBox="-143 145 512 512" xml:space="preserve">
                 <path d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M215.2,361.2
 	c0.1,2.2,0.1,4.5,0.1,6.8c0,69.5-52.9,149.7-149.7,149.7c-29.7,0-57.4-8.7-80.6-23.6c4.1,0.5,8.3,0.7,12.6,0.7
@@ -71,7 +71,7 @@ const Footer = () => {
 
         <div className="socials-list lg:hidden gap-3 justify-center items-center mt-7 flex">
           <a href='#' className='linkedIn '>
-            <svg fill="#fff" height="35" width="35" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            <svg className='w-9 h-9 fill-current text-white hover:text-primary transition-all duration-500 delay-100 ' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
               viewBox="-143 145 512 512" xml:space="preserve">
               <path d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M41.4,508.1H-8.5V348.4h49.9
 	V508.1z M15.1,328.4h-0.4c-18.1,0-29.8-12.2-29.8-27.7c0-15.8,12.1-27.7,30.5-27.7c18.4,0,29.7,11.9,30.1,27.7
@@ -81,12 +81,12 @@ const Footer = () => {
             </svg>
           </a>
           <a href='#' className='facebook '>
-            <svg fill="#fff" width="35" height="35" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg className='w-9 h-9 fill-current text-white hover:text-primary transition-all duration-500 delay-100 ' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 2.03998C6.5 2.03998 2 6.52998 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.84998C10.44 7.33998 11.93 5.95998 14.22 5.95998C15.31 5.95998 16.45 6.14998 16.45 6.14998V8.61998H15.19C13.95 8.61998 13.56 9.38998 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96C15.9164 21.5878 18.0622 20.3855 19.6099 18.57C21.1576 16.7546 22.0054 14.4456 22 12.06C22 6.52998 17.5 2.03998 12 2.03998Z" />
             </svg>
           </a>
           <a href='#' className='twitter '>
-            <svg fill="#fff" height="35" width="35" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            <svg className='w-9 h-9 fill-current text-white hover:text-primary transition-all duration-500 delay-100 ' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
               viewBox="-143 145 512 512" xml:space="preserve">
               <path d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M215.2,361.2
 	c0.1,2.2,0.1,4.5,0.1,6.8c0,69.5-52.9,149.7-149.7,149.7c-29.7,0-57.4-8.7-80.6-23.6c4.1,0.5,8.3,0.7,12.6,0.7
