2f7fd738683e9e6c49aeb5deca217b9e3ef2b977
Habib <yusufhabib3@gmail.com>
2026-05-03 21:17:55 +0100

    added navbar hide feature when srolling up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,31 @@ import React, { useState } from 'react'
 const Header = () => {
 
     const [toggleMenu, setToggleMenu] = useState(false);
+    const [prevScrollY, setPrevScrollY] = useState(0);
+    const [showNavBar, setShowNavBar] = useState(true);
+
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+        if (currentScrollY > prevScrollY) {
+            setShowNavBar(false);
+        } else {
+            setShowNavBar(true);
+        }
+        setPrevScrollY(currentScrollY);
+    };
+
+    React.useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [prevScrollY]);
 
     return (
         <header className=' flex flex-col relative w-full px-padding-sm h-auto my-padding-sm items-center
         Stablet:px-padding-md lg:px-padding-lg'>
-            <nav className='bg-white fixed left-0 right-0 px-padding-sm z-101 top-0 w-full h-[105px] flex justify-between py-padding-sm 
-            lg:px-padding-lg '>
+            <nav className={`bg-white fixed left-0 right-0 px-padding-sm z-101 top-0 w-full h-[105px] flex justify-between py-padding-sm 
+            lg:px-padding-lg items-center transition-transform duration-300 ease-in-out ${showNavBar ? 'translate-y-0' : '-translate-y-full'}`}>
                 <div className='flex items-center'>
                     <div className=''>
                         <img
@@ -19,7 +38,8 @@ const Header = () => {
                 </div>
                 <ul className={`absolute w-2/3 top-[105px] bg-inherit flex flex-col h-screen items-center 
                 font-normal text-[18px] transition-all transform ease-in-out duration-200 gap-6
-                ${!toggleMenu ? '-right-full' : 'right-0 bg-primary'} 
+                ${!toggleMenu ? '-right-full' : 'right-0 bg-primary'}
+                ${!showNavBar ? 'hidden' : 'block'}
                 md:w-1/3 lg:flex-row lg:static lg:w-auto lg:h-auto lg:gap-0  `}>
                     <li className='nav-li lg:mr-9 '><a href="#">About us</a></li>
                     <li className='nav-li lg:mr-9 '><a href="#">Services</a></li>
